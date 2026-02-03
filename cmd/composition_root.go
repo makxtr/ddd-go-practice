3f2f3902119838c76e762b0b1d31a3477b063faa
makx <makx.work@gmail.com>
2026-02-03 21:25:04 +0200

    init

---
+++ cmd/composition_root.go
@@ -0,0 +1,13 @@
+package cmd
+
+type CompositionRoot struct {
+	configs Config
+
+	closers []Closer
+}
+
+func NewCompositionRoot(configs Config) *CompositionRoot {
+	return &CompositionRoot{
+		configs: configs,
+	}
+}
